ecf0ac5eda53b886858564194a3955df1cdac35d
julianyanez <julian.yanez@claro.com.ar>
2020-10-09 16:19:10 -0300

    Elimino carpeta duplicada

--- desafio1-CommentTicket/src/main/java/ar/edu/teclab/desafioback/entity/Comment.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.teclab.desafioback.entity;
-
-import lombok.Getter;
-
-@Getter
-public class Comment {
-    private String body;
-    private int author_id;
-}
--- desafio1-CommentTicket/src/main/java/ar/edu/teclab/desafioback/entity/Ticket.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.teclab.desafioback.entity;
-
-import lombok.Getter;
-
-@Getter
-public class Ticket {
-    private Comment comment;
-}
